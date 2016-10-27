7a8345c193e7b64dc2378c22c83df26223d4d416
Project-MAR <atitep.ram@gmail.com>
2016-10-27 22:19:21 +0700

    Cross Port Message Passing

---
+++ Multiple Port/Server-Test-MultiPort.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import socket
+
+g_clients  = []
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    
+    def get(self):
+        print('')
+        print ('webpage connect')
+        self.render("index-MultiPort.html")
+
+class WebWSHandler(tornado.websocket.WebSocketHandler):
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in g_clients:
+            print('append connection')
+            g_clients.append(self)
+        print('')
+        print ('Websocket Connect')
+        print('Client number: ' + str(len(g_clients)))
+        #print(g_clients)
+      
+    def on_message(self, message):
+        print ('message received: %s' % message)
+
+        if len(g_clients) > 0:
+            print('Foward to Port 9990')
+            self.send_message_to_all(message)
+            #self.forward_message(message)
+            
+            print('>>> Client List <<<')
+            for c in g_clients:
+                print(str(c))
+
+
+    def on_close(self):
+        if self in g_clients:
+            print ('websocket closed: ' + str(self))
+            g_clients.remove(self)
+        
+        print('>>> Client List <<<')
+        for c in g_clients:
+            print(str(c))
+
+    def send_message_to_all(self, message):
+        for c in g_clients:
+            c.write_message(message)
+
+    def forward_message(self, message):
+        g_clients[0].write_message(message)
+
+WebApp = tornado.web.Application([
+    (r'/', IndexHandler),
+    (r'/web_ws', WebWSHandler)
+    ])
+if __name__ == "__main__":
+
+    #tornado.options.parse_command_line()
+    
+    #sockets = tornado.netutil.bind_sockets(8880)
+    #tornado.process.fork_processes(0)
+    http_server = tornado.httpserver.HTTPServer(WebApp)
+    #http_server.add_sockets(sockets)
+    #http_server.listen(80)
+    print('bind 80 8880 9990')
+    http_server.bind(80)
+    http_server.bind(8880)
+    http_server.bind(9990)
+    http_server.start(num_processes = 1)
+
+    myIP = socket.gethostbyname(socket.gethostname())
+    print ('*** Websocket Server Started at %s***' % myIP)
+    
+    #tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.instance().start()
+
+    
