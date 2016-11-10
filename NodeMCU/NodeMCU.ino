bba10d743b937a352d157d65f4df984609467946
Project-MAR <atitep.ram@gmail.com>
2016-11-11 01:02:21 +0700

    SSL with AC Sense, work only with PC Browser.

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -5,17 +5,71 @@
 #include <Hash.h>
 #include <string.h>
 
-int LightStatus = 0;
-
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
-int count = 0;
-char Apple[30];
-char buffer [30];
+
+#define ON      HIGH
+#define OFF     LOW
+#define STATE_A HIGH
+#define STATE_B LOW
+#define TRUE    HIGH
+#define FALSE   LOW
+
+int LoadStatus = ON;
 char nodeCMD[]= "wb.toggle.l.1";
 
+int switchState = ON;
+
+// Port Define
 int relayA = 4;
 int relayB = 5;
+int AC_Sense = 15;
+
+int AC_Sense_readState = ON;
+
+void toggleLoad(int *LoadStatus, int *switchState) {
+  if(*switchState == STATE_A) {
+      if(*LoadStatus == ON) {
+          *LoadStatus = OFF;
+          Serial.printf("CASE: 1\n");
+          Serial.printf("Load OFF\n");
+          digitalWrite(relayA, HIGH);
+          digitalWrite(relayB, LOW);
+          delay(500);
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, LOW);
+    } else {
+          *LoadStatus = ON;
+          Serial.printf("CASE: 2\n");
+          Serial.printf("Load ON\n");
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, HIGH);
+          delay(500);
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, LOW);
+    }
+  }else {
+      if(*LoadStatus == ON) {
+          *LoadStatus = OFF;
+          Serial.printf("CASE: 3\n");
+          Serial.printf("Load OFF\n");
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, HIGH);
+          delay(500);
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, LOW);
+      } else {
+          *LoadStatus = ON;
+          Serial.printf("CASE: 4\n");
+          Serial.printf("Load ON\n");
+          digitalWrite(relayA, HIGH);
+          digitalWrite(relayB, LOW);
+          delay(500);
+          digitalWrite(relayA, LOW);
+          digitalWrite(relayB, LOW);
+      }
+  }
+}
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
 
@@ -30,27 +84,9 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
         
         case WStype_TEXT:
             if(strcmp((const char*)payload, nodeCMD) == 0){
-                Serial.printf("[WS] Receive: %s\n", payload);
-                
-                if(LightStatus){
-                  LightStatus = 0;
-                  Serial.printf("Light ON\n");
-                  digitalWrite(relayA, LOW);
-                  digitalWrite(relayB, HIGH);
-                  delay(500);
-                  digitalWrite(relayA, LOW);
-                  digitalWrite(relayB, LOW);
-   
-                }else{
-                  LightStatus = 1;
-                  Serial.printf("Light OFF\n");
-                  digitalWrite(relayA, HIGH);
-                  digitalWrite(relayB, LOW);
-                  delay(500);
-                  digitalWrite(relayA, LOW);
-                  digitalWrite(relayB, LOW);
-                }
-              }  
+                Serial.printf("\n[WS] Receive: %s\n", payload);
+                toggleLoad(&LoadStatus, &switchState);
+              }
             break;
             
         case WStype_BIN:
@@ -72,16 +108,27 @@ void setup() {
     pinMode(relayB, OUTPUT);
     digitalWrite(relayA, LOW);
     digitalWrite(relayB, LOW);
+
+    pinMode(AC_Sense, INPUT);
     delay(500);
-    
+
+    switchState = digitalRead(AC_Sense);
+    if(switchState) {
+        Serial.printf("switch state is now: A\n");
+        LoadStatus = ON;
+    }else {
+        Serial.printf("switch state is now: B\n");
+        LoadStatus = OFF;
+    }
+      
       for(uint8_t t = 4; t > 0; t--) {
           Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
           Serial.flush();
           delay(1000);
       }
 
-    WiFiMulti.addAP("SSID1", "PASS1");
-    WiFiMulti.addAP("SSID2", "PASS2");
+    WiFiMulti.addAP("WifiNaam", "qawsEDRF");
+    //WiFiMulti.addAP("MIND-WIFI", "PASS2");
 
     Serial.printf("Connecting Wifi...\n");
     
@@ -101,9 +148,29 @@ void setup() {
     webSocket.beginSSL("192.168.1.90", 8880, "/web_ws");
     
     //webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
-    webSocket.onEvent(webSocketEvent);
+    webSocket.onEvent(webSocketEvent); 
 }
 
 void loop() {
+    // Web Socket loop
     webSocket.loop();
+
+    AC_Sense_readState = digitalRead(AC_Sense);
+    if((AC_Sense_readState != LoadStatus)) {
+      // toggle switch state
+      if(switchState == STATE_A) {
+        switchState = STATE_B;
+        Serial.printf("switch state is now: B\n");
+      }else {
+        switchState = STATE_A;
+        Serial.printf("switch state is now: A\n");
+      }      
+          
+      LoadStatus = AC_Sense_readState;
+      if(LoadStatus)
+        Serial.printf("Load ON\n");
+      else
+        Serial.printf("Load OFF\n");
+   }
 }
+
